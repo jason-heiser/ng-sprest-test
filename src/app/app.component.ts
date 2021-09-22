f8b3cb68d9f35baa65314b1cdad099a52c22d518
Jason Heiser <jason@heiser.org>
2021-09-22 11:24:51 -0500

    Use gd-sprest’s Web object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Web } from 'gd-sprest';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-sprest-test';
+  web = Web('http://localhost/').execute(console.log, console.error);
 }
